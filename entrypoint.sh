1e694a759cf3afa05890084ef7e9b1795c3a64ba
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-04-01 15:42:21 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #author https://github.com/developeranaz (don't delete this)
 #refreshing Rclone
-#rclone version
+rclone version
 #serving to port 8080
-#rclone serve http $STORAGENAME: --addr :$PORT
+rclone serve http 1sundaran1: --addr :$PORT
 #docker run -it rclone/rclone serve http 1sundaran1: --addr :$PORT
-docker run -it pfidr/rclone serve http 1sundaran1: --addr :$PORT
+#docker run -it pfidr/rclone serve http 1sundaran1: --addr :$PORT
