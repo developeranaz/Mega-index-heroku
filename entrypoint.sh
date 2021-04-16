d096d007b6861d055ce761959c9d3676207ce593
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-04-17 00:59:12 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 #author https://github.com/developeranaz (don't delete this)
 #refreshing Rclone
 rclone version
+rclone copy /donate-developeranaz.txt $CLOUDNAME:
 #serving to port 8080
 rclone serve http $CLOUDNAME: --addr :$PORT
-
