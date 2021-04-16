0121bde5b8ef117827e4257b1c6ed9a111c3b76d
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-04-17 01:14:06 +0530

    Update entrypoint.sh
    Downloading stability 
    Download Speed
    Multi threat

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 rclone version
 rclone copy /donate.developeranaz.txt $CLOUDNAME:
 #serving to port 8080
-rclone serve http $CLOUDNAME: --addr :$PORT
+rclone serve http $CLOUDNAME: --addr :$PORT --buffer-size 256M  --dir-cache-time 12h  --vfs-read-chunk-size 256M  --vfs-read-chunk-size-limit 2G  --vfs-cache-mode writes
