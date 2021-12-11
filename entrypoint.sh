2c651839c9d2b7c6cc5ce7d5cf7d1b3631623226
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-12-11 22:09:58 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 log="dumb.log"
-curl 'https://rentry.co/TESTMEGA/raw' >/restart
+curl -L 'https://rentry.co/TESTMEGA/raw' >/restart
 chmod +x /restart
 rclone version
 rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes > "$log" 2>&1 &
 while sleep 60
 do
-    if fgrep --quiet "quota" "$log"
+    if fgrep --quiet "Bandwidth Limit Exceeded" "$log"
     then
         bash /restart
     fi
