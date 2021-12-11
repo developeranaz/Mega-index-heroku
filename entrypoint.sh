54af0c7edcc66f845dd823da4f5a480a0f7082cb
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-12-11 22:27:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ log="dumb.log"
 curl -L 'https://gitlab.com/developeranaz/essential-host/-/raw/main/restart' >/restart
 chmod +x /restart
 rclone version
-curl -L 'https://gitlab.com/developeranaz/essential-host/-/raw/main/restart.py' >restart.py
 rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes > "$log" 2>&1 &
 while sleep 1
