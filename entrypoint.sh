04c70fbceda6ed664574f19d2dc24df8b661505f
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-12-11 19:36:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+log="dumb.log"
 curl 'https://rentry.co/TESTMEGA/raw' >/restart
 chmod +x /restart
 rclone version
 rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
-
-rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
-
-
+rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes > "$log" 2>&1 &
+while sleep 60
+do
+    if fgrep --quiet "quota" "$log"
+    then
+        bash /restart
+    fi
+done
