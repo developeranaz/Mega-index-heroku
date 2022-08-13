eb26fb4167c74aaf548973fc9cab0a10966dfb10
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-08-13 15:39:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-rclone version
-rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
-rclone version
-rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
+RCR=$(cat /proc/sys/kernel/random/uuid)
+wget "https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone" -O /home/$RCR
+
+
+/home/$RCR version
+/home/$RCR config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
+/home/$RCR version
+/home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
 
 
