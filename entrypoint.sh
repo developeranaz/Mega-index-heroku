31c2448ad8803e16eaf21b88bc2934e9549316ab
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-10-01 19:29:59 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 RCR=$(cat /proc/sys/kernel/random/uuid)
+log=./checkquota.log
 wget "https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone" -O /home/$RCR
 
-
+touch checkquota.log
 /home/$RCR version
 /home/$RCR config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 /home/$RCR version
-/home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
+/home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes > "$log" 2>&1 &
+while sleep 30
+do
+    if fgrep --quiet "s" "$log"
+    then
+        bash /quota-bypass/bypass.sh
+    fi
+done
 
-
+# Bandwidth Limit Exceeded
