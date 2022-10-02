f6e604ca768412cc637265cd7ea831c53e8f1ace
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-10-03 00:35:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ RCR=$(cat /proc/sys/kernel/random/uuid)
 touch checkquota.log
 log=checkquota.log
 wget "https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone" -O /home/$RCR
-curl -L "https://raw.githubusercontent.com/developeranaz/Mega-index-heroku/main/quota-bypass/login.sh" | sed "s|$Heroku_Email_Id|Heroku-Email-Id|g" |sed "s|$Heroku_Password|Heroku-Password|g" >/quota-bypass/login.sh
+#curl -L "https://raw.githubusercontent.com/developeranaz/Mega-index-heroku/main/quota-bypass/login.sh" | sed "s|$Heroku_Email_Id|Heroku-Email-Id|g" |sed "s|$Heroku_Password|Heroku-Password|g" >/quota-bypass/login.sh
 chmod +x /home/$RCR
 chmod +x /Mega-index-heroku/quota-bypass/init.sh
 chmod +x /Mega-index-heroku/quota-bypass/login.sh
