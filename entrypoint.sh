aa4378abd5ff7443eccf7a5337aaaf5e002ed087
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-10-03 00:24:24 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ touch /Mega-index-heroku/quota-bypass/checkquota.log
 /home/$RCR version
 /home/$RCR config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 /home/$RCR version
+echo 'okkkkk1111111111111111111111111111111111111111111111'
 /home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes -v > "$log" 2>&1 &
 while sleep 20
 do
