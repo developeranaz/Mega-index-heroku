2832b63c861825d6d6a83b3808bac403a1a8190b
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-10-03 06:35:34 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,16 +14,15 @@ chmod +x /Mega-index-heroku/quota-bypass/bypass.sh
 touch /Mega-index-heroku/quota-bypass/checkquota.log
 /home/$RCR version
 /home/$RCR config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
-/home/$RCR version
-echo 'okkkkk1111111111111111111111111111111111111111111111'
+echo 'Logs from entrypoint 1111111111111111111111111111111111111111111111'
 /home/$RCR serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes -v > "$log" 2>&1 &
 while sleep 20
 do
-    if fgrep --quiet "c" "$log"
+    if fgrep --quiet "Bandwidth Limit Exceeded" "$log"
     then
         cd /Mega-index-heroku/quota-bypass
         bash bypass.sh
     fi
 done
 
-#Bandwidth Limit Exceeded
+#
